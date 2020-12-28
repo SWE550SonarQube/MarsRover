fdd3bbda9a6dcfd9a3cee2cd50adf54fac951225
sabri <sabri_51@hotmail.com>
2020-12-28 18:52:35 +0300

    initial commit

--- MarsRoverTest/RoverExceptionTest.cs
+++ MarsRoverTest/RoverExceptionTest.cs
@@ -10,7 +10,7 @@ namespace MarsRoverTest
         {
             var rover = new Rover("4 6");
             Assert.Throws<ArgumentOutOfRangeException>(() =>   rover.SetCurrentPosition("5 4 N"));
-        }
+        } 
 
         [Fact]
         public void SetPosition__Limit2_6__Position_1_6_S__OutOfLimit_Exception()
@@ -18,7 +18,7 @@ namespace MarsRoverTest
             var rover = new Rover("4 6");
             rover.SetCurrentPosition("1 6 S");
             Assert.Throws<ArgumentOutOfRangeException>(() =>   rover.SetCurrentPosition("4 7 N"));
-
+ 
         }
         [Fact]
         public void Command_LMLMRMLMRMM_Limit4_6__Position_4_3_E__OutOfLimit_Exception()
